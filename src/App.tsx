93057a7939271bec95d29bffeb93aeb8e17025c1
mohamedabdiah <mohamedabdikaniahmed@gmail.com>
2025-05-24 00:18:48 +0300

    Done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center text-blue-600 font-bold'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
